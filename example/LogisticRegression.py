b81dd6b0135aa25091532715a45462a8bb7c7ebf
Duong The Cuong <duongthecuongbk227@gmail.com>
2019-05-09 23:09:24 +0700

    update ebook

--- example/LogisticRegression.py
+++ example/LogisticRegression.py
@@ -126,3 +126,24 @@ lam = 0.0001
 w, loss_hist = logistic_regression_with_decay(w_init, Xbar, y, lam, eta = 0.05, nepoches = 500)
 print('Solution of Logistic Regression: ',w)
 print('Final loss: ',loss(w, Xbar, y, lam))
+
+
+
+
+#include<sys/types.h>
+#include<stdio.h>
+#include<unistd.h>
+
+int main(){
+    if( fork() == 0){
+        wait(NULL);
+        calc();
+        
+
+    } else {
+        wait(NULL);
+        fork();
+        wait(NULL);
+    }
+        
+}
