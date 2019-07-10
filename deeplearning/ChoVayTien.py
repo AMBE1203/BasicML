966ff8366b6de7e02b36ee0a5e58f1c0d352cfc2
duong the cuong <duongthecuongbk227@gmail.com>
2019-07-10 11:09:54 +0700

    u

--- deeplearning/ChoVayTien.py
+++ deeplearning/ChoVayTien.py
@@ -9,13 +9,60 @@ data = pd.read_csv('./data/data_logistic.csv').values
 N = data.shape[0]
 d = data.shape[1]
 
-print(d)
 X = data[:, 0:d-1].reshape(-1, d-1)
 y = data[:, 2].reshape(-1, 1)
 
 #bias trick
 Xbar = np.concatenate((np.ones((N, 1)), X), axis = 1)
 
+
+
+def sigmoid(S):
+    return 1/(1 + np.exp(-S))
+
+def prob(w, X):
+    return sigmoid(X.dot(w))
+
+def loss(w, X, y, lam):
+    z = prob(w, X)
+    return -np.mean(y*np.log(z) + (1-y)*np.log(1-z)) + 0.5*lam/X.shape[0]*np.sum(w*w)
+
+def logistic_regression(w_init, X, y, lam = 0.01, lr = 0.1, nepoches = 2000):
+    N, d = X.shape[0], X.shape[1]
+    w = w_old = w_init
+    loss_hist = [loss(w_init, X, y, lam)]
+    ep = 0
+    while ep < nepoches:
+        ep += 1
+        mix_id = np.random.permutation(N)
+        for i in mix_id:
+            xi = X[i]
+            yi = y[i]
+            zi = sigmoid(xi.dot(w))
+
+            w = w - lr*((zi-yi)*xi + lam*w)
+        loss_hist.append(loss(w, X, y, lam))
+        if np.linalg.norm(w - w_old)/d < 1e-6:
+            break
+        w_old = w
+    return w, loss_hist
+
+w_init = np.random.randn(Xbar.shape[1])
+
+lam = 0.0001
+w, loss_hist = logistic_regression(w_init, Xbar, y, lam, lr = 0.05, nepoches = 500)
+print(w)
+print(loss(w, Xbar, y, lam))    
+
+
+plt.plot(loss_hist)
+plt.xlabel('Iteration number')
+plt.ylabel('Loss value')
+plt.show()
+
+# Vẽ đường phân cách.
+t = 0.5
+plt.plot((4, 10),(-(w[0]+4*w[1]+ np.log(1/t-1))/w[2], -(w[0] + 10*w[1]+ np.log(1/t-1))/w[2]), 'g')
 # Vẽ data bằng scatter
 plt.scatter(X[:10, 0], X[:10, 1], c='red', edgecolors='none', s=30, label='cho vay')
 plt.scatter(X[10:, 0], X[10:, 1], c='blue', edgecolors='none', s=30, label='từ chối')
